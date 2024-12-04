eb42ff3d5faa7161b369b65d3b8cf5f4c2c04e03
likithsd <likilikith809@gmail.com>
2024-12-04 22:09:18 +0530

    Create Choice.java

---
+++ Choice.java
@@ -0,0 +1,14 @@
+package com.example.backend;
+
+public class Choice {
+    private String choice;
+
+    // Getters and Setters
+    public String getChoice() {
+        return choice;
+    }
+
+    public void setChoice(String choice) {
+        this.choice = choice;
+    }
+}
